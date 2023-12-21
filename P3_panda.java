bbb0c37f542b0205cc2fb7179146a046d94c6c1e
Kaustubh Bhole <145138804+kaustubh2802@users.noreply.github.com>
2023-12-21 22:57:36 +0530

    Create P3_panda.java
    Write a program to implement a package

---
+++ P3_panda.java
@@ -0,0 +1,19 @@
+Write a program to implement a package
+package p;
+public class gautu{
+public void m1() {
+System.out.println("m1 method gautu class");
+}
+}
+
+
+package p1;
+import p.*;
+public class panda
+{
+public static void main(String args[])
+{
+gautu t=new gautu();
+t.m1();
+}
+}
