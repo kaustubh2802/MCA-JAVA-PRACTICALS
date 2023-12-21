1fee5c0d78e0cc9355c78548f21fec8494c297ca
Kaustubh Bhole <145138804+kaustubh2802@users.noreply.github.com>
2023-12-21 23:28:12 +0530

    javascript Fibonacci Series Program using While Loop

---
+++ JS_FIBONACCI.js
@@ -0,0 +1,19 @@
+PracticalNo :-16
+Write and Execute javascript Fibonacci Series Program using While Loop
+//Input:
+<html>
+<body>
+<pid="s"></p>
+<script>
+var a=0,b=1,n=5,c,i=0;while(i<n)
+{
+
+c=a+b;
+document.write("<br>"+c+"");a=b;
+b=c;i++;
+}
+
+</script>
+</body>
+ 
+</html>
